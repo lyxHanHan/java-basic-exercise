006735f1734044bbbac7a05f68a513bcfe4da46d
liyixuan <13069579956@163.com>
2020-09-09 07:17:08 +0800

    liyixuan feat:full in the function

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -13,6 +13,57 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        List<String> inputStrList = getInputStrList();
+        if (null == inputStrList) {
+            System.exit(0);
+        }
+
+        List<String> collect = inputStrList.stream()
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted(Comparator.comparing(String::trim))
+                .collect(Collectors.toList());
+        System.out.println("处理结果:" + collect);
+
     }
+
+    public static List<String> getInputStrList(){
+        Scanner WordList = new Scanner(System.in);
+        System.out.println("请输入英文单词, 使用英文逗号(,)隔开 回车键结束输入:");
+        String inputStr = WordList.nextLine();
+        System.out.println("输入内容:" + inputStr);
+        
+        if (null == inputStr || inputStr.length() <= 0) {
+            System.out.println("输入的内容不能为空");
+            return null;
+        }
+
+        String[] split = inputStr.split(",");
+        if (split.length <= 0) {
+            System.out.println("输入的内容不能为空");
+            return null;
+        }
+
+        List<String> inputStrList = new ArrayList<String>(Arrays.asList(split));
+        if (inputStrList.size() <= 0) {
+            System.out.println("输入的内容不能为空");
+            return null;
+        }
+
+        if (inputStrList.contains("")) {
+            System.out.println("input not valid");
+            return null;
+        }
+
+        inputStrList.stream().forEach(r -> {
+            if (r.matches("^[0-9]*$")) {
+                System.out.println("input not valid");
+                System.exit(0);
+            }
+        });
+
+        return inputStrList;
+    }
+
+    
 }
